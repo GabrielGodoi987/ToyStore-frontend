cba44fd344a052dc20bfd16be7a5282f9ff36cab
gabrielgodoi987 <gabrielgodoi452@gmail.com>
2025-03-22 11:58:09 -0300

    initial dark & lihght mode

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,11 +1,39 @@
 <template>
     <div>
+        <h1>admin layout</h1>
+        <button @click="togleDarkLightMode"
+            class="px-4 py-2 rounded-2xl shadow-md font-semibold text-white transition-all duration-300 ease-in-out"
+            :class="isDarkOrLight
+                ? 'bg-slate-950'
+                : 'bg-secondary'">
+            {{ isDarkOrLight ? "Modo escuro" : "Modo claro" }}
+        </button>
+    </div>
+    <div>
         <RouterView />
     </div>
 </template>
 
 <script lang="ts" setup>
+import { ref } from 'vue';
+
+
+const isDarkOrLight = ref<boolean>(false);
+const currentTheme = ref<'dark' | 'light'>('dark');
+const togleDarkLightMode = () => {
+    isDarkOrLight.value = !isDarkOrLight.value;
+    if (currentTheme.value == 'dark') {
+        currentTheme.value = 'light'
+    } else {
+        currentTheme.value = 'dark';
+    }
+    document.documentElement.style.setProperty('--color-theme', currentTheme.value)
+}
 
 </script>
 
-<style></style>
+<style>
+:root {
+    color-scheme: var(--color-theme);
+}
+</style>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -3,11 +3,13 @@
         <h1 class="text-3xl font-bold">
             {{ msg }}
         </h1>
+
     </div>
 </template>
 
 <script lang="ts" setup>
+
 const msg = "Hello world!";
 </script>
-
-<style></style>
+<style>
+</style>
